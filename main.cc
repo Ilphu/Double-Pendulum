de803fc26c4ba4386e86659fea5713867fd4d4b9
Garrett <99564508+Ilphu@users.noreply.github.com>
2024-12-29 19:52:02 -0500

    double pendulum working

--- main.cc
+++ main.cc
@@ -62,11 +62,11 @@ public:
 
     struct Pixel get_pixel(const struct RectPoint& pt);
     void draw_pixel(const struct RectPoint& pt, const struct Pixel& px);
-    void draw_line(struct RectPoint& pt0, struct RectPoint& pt1);
+    void draw_line(struct RectPoint pt0, struct RectPoint pt1);
 
     void clear_graph();
 
-    void write_image();
+    void write_image(const string& file_name);
 private:
 
     int _img_width = 100;
@@ -121,7 +121,7 @@ void Graph::draw_pixel(const struct RectPoint& pt, const struct Pixel& px) {
     *(_pix + 2) = px.b;
 }
 
-void Graph::draw_line(struct RectPoint& pt0, struct RectPoint& pt1) {
+void Graph::draw_line(struct RectPoint pt0, struct RectPoint pt1) {
     struct RectPoint pt;
     struct RectPoint swap;
     double dx = pt1.x - pt0.x;
@@ -173,8 +173,8 @@ void Graph::clear_graph() {
     }
 }
 
-void Graph::write_image() {
-    stbi_write_png("Graph.png", _img_width, _img_height, CHANNELS, _img, _img_width * CHANNELS);
+void Graph::write_image(const string& file_name) {
+    stbi_write_png(file_name.c_str(), _img_width, _img_height, CHANNELS, _img, _img_width * CHANNELS);
 }
 
 
@@ -201,9 +201,11 @@ public:
 
     vector<double> G(const vector<double>& y, const double& t);
     vector<double> RK4_step(const vector<double>& y, const double& t, const double& dt);
-    vector<RectPoint> get_position() const;
+    void update_state(double t, double dt);
+    void update_d_omega();
+    vector<RectPoint> get_position(bool state) const;
 private:
-    const double g = 9.80665;
+    const double g = 9.806;
 
     double _mass1;
     double _length1;
@@ -211,7 +213,14 @@ private:
     double _mass2;
     double _length2;
 
-    vector<double> _state;
+    double _theta1;
+    double _theta2;
+    double _omega1;
+    double _omega2;
+    double _d_omega1;
+    double _d_omega2;
+
+    vector<double> _state; // theta1 theta2, omega1 omega2,
 };
 
 DoublePendulum::DoublePendulum() {}
@@ -225,10 +234,17 @@ DoublePendulum::DoublePendulum(const double& theta1,  const double& theta2,
     _mass2 = mass2;
     _length2 = length2;
 
-    _state.push_back(0.0);
-    _state.push_back(0.0);
-    _state.push_back(theta1);
-    _state.push_back(theta2);
+    _theta1 = theta1;
+    _theta2 = theta2;
+    _omega1 = 0.0;
+    _omega2 = 0.0;
+    _d_omega1 = 0.0;
+    _d_omega2 = 0.0;
+
+    _state.push_back(_theta1);
+    _state.push_back(_theta2);
+    _state.push_back(_omega1);
+    _state.push_back(_omega2);
 }
 
 DoublePendulum::~DoublePendulum() {}
@@ -243,33 +259,55 @@ void DoublePendulum::set_length1(const double& new_length1) {_length1 = new_leng
 void DoublePendulum::set_mass2(const double& new_mass2) {_mass2 = new_mass2;}
 void DoublePendulum::set_length2(const double& new_length2) {_length2 = new_length2;}
 void DoublePendulum::set_state(const vector<double>& new_state) {_state[0] = new_state[0]; _state[1] = new_state[1]; 
-                                                          _state[2] = new_state[2]; _state[3] = new_state[3];}
+                                                                 _state[2] = new_state[2]; _state[3] = new_state[3];}
 
 vector<double> DoublePendulum::G(const vector<double>& y, const double& t) {
-    double angular_v1 = y[0];
-    double angular_v2 = y[1];
-    double theta1 = y[2];
-    double theta2 = y[3];
+    double theta1 = y[0];
+    double theta2 = y[1];
+    double omega1 = y[2];
+    double omega2 = y[3];
+    double num1;
+    double num2;
+    double num3;
+    double num4;
+    double den;
 
-    double m11 = (_mass1 + _mass2) * _length1;
-    double m12 = (_mass2 * _length2 * cos(theta1 - theta2));
-    double m21 = (_length1 * cos(theta1 - theta2));
-    double m22 = (_length2);
+    // double m11 = (_mass1 + _mass2) * _length1;
+    // double m12 = (_mass2 * _length2 * cos(theta1 - theta2));
+    // double m21 = (_length1 * cos(theta1 - theta2));
+    // double m22 = (_length2);
 
-    double det_m = (m11 * m22) - (m12 * m21);
+    // double det_m = (m11 * m22) - (m12 * m21);
 
-    double f1 = (-1 * _mass2 * _length2 * angular_v2 * angular_v2 * sin(theta1 - theta2)) -
-                ((_mass1 + _mass2) * g * sin(theta1));
-    double f2 = (_length1 * angular_v1 * angular_v2 * sin(theta1 - theta2)) * (g * sin(theta2));
+    // double f1 = (-1 * _mass2 * _length2 * angular_v2 * angular_v2 * sin(theta1 - theta2)) -
+    //             ((_mass1 + _mass2) * g * sin(theta1));
+    // double f2 = (_length1 * angular_v1 * angular_v2 * sin(theta1 - theta2)) * (g * sin(theta2));
 
-    double accel1 = (1 / det_m) * ((m22 * f1) - (m21 * f2));
-    double accel2 = (1 / det_m) * ((-1 * m12 * f1) + (m11 * f2));
+    // double accel1 = (1 / det_m) * ((m22 * f1) - (m21 * f2));
+    // double accel2 = (1 / det_m) * ((-1 * m12 * f1) + (m11 * f2));
+
+    double f1 = omega1;
+    double f2 = omega2;
+
+    num1 = -g * ((2 * _mass1) + _mass2) * sin(theta1);
+    num2 = -_mass2 * g * sin(theta1 - (2 * theta2));
+    num3 = -2 * sin(theta1 - theta2) * _mass2;
+    num4 = ((omega2 * omega2 * _length2) + (omega1 * omega1 * _length1 * cos(theta1 - theta2)));
+    den  = (_length1 * ((2 * _mass1) + _mass2 - (_mass2 * cos((2 * theta1) - (2 * theta2)))));
+    double f3 = (num1 + num2 + (num3 * num4)) / den;
+
+    num1 = 2 * sin(theta1 - theta2);
+    num2 = (omega1 * omega1 * _length1 * (_mass1 + _mass2));
+    num3 = (g * (_mass1 + _mass2) * cos(theta1));
+    num4 = omega2 * omega2 * _length1 * _mass2 * cos(theta1 - theta2);
+    den  = (_length2 * ((2 * _mass1) + _mass2 - (_mass2 * cos((2 * theta1) - (2 * theta2)))));
+    double f4 = (num1 * (num2 + num3 + num4)) / den;
 
     vector<double> new_state;
-    new_state.push_back(accel1);
-    new_state.push_back(accel2);
-    new_state.push_back(angular_v1);
-    new_state.push_back(angular_v2);
+    new_state.push_back(f1);
+    new_state.push_back(f2);
+    new_state.push_back(f3);
+    new_state.push_back(f4);
     return new_state;
 }
 
@@ -295,24 +333,63 @@ vector<double> DoublePendulum::RK4_step(const vector<double>& y, const double& t
         temp[i] += k3[i] * dt;
     }
     vector<double> k4 = G(temp, t + dt);
-
+    
     for (int i = 0; i < k4.size(); i++) {
-        new_state[i] = dt * (k1[i] + (2 * k2[i]) + (2 * k3[i]) + k4[i]) / 6;
+        //cout << "here" << endl;
+        new_state.push_back(dt * (k1[i] + (2 * k2[i]) + (2 * k3[i]) + k4[i]) / 6);
     }
 
     return new_state;
 }
 
-vector<RectPoint> DoublePendulum::get_position() const {
+void DoublePendulum::update_state(double t, double dt) {
+    vector<double> new_state = RK4_step(_state, t, dt);
+    for (int i = 0; i < _state.size(); i++) {
+        _state[i] += new_state[i];
+        //cout << new_state[i] << endl;
+    }
+}
+
+void DoublePendulum::update_d_omega() {
+    double num1 = -g * ((2 * _mass1) + _mass2) * sin(_theta1);
+    double num2 = -_mass2 * g * sin(_theta1 - (2 * _theta2));
+    double num3 = -2 * sin(_theta1 - _theta2) * _mass2;
+    double num4 = ((_omega2 * _omega2 * _length2) + (_omega1 * _omega1 * _length1 * cos(_theta1 - _theta2)));
+    double den  = (_length1 * ((2 * _mass1) + _mass2 - (_mass2 * cos((2 * _theta1) - (2 * _theta2)))));
+
+    _d_omega1 = (num1 + num2 + (num3 * num4)) / den;
+    
+    num1 = 2 * sin(_theta1 - _theta2);
+    num2 = (_omega1 * _omega1 * _length1 * (_mass1 + _mass2));
+    num3 = (g * (_mass1 + _mass2) * cos(_theta1));
+    num4 = _omega2 * _omega2 * _length1 * _mass2 * cos(_theta1 - _theta2);
+    den  = (_length2 * ((2 * _mass1) + _mass2 - (_mass2 * cos((2 * _theta1) - (2 * _theta2)))));
+
+    _d_omega2 = (num1 * (num2 + num3 + num4)) / den;
+
+    _omega1 += _d_omega1;
+    _omega2 += _d_omega2;
+    _theta1 += _omega1;
+    _theta2 += _omega2;
+}
+
+vector<RectPoint> DoublePendulum::get_position(bool state) const {
     struct RectPoint pt1;
     struct RectPoint pt2;
 
-    pt1.x = sin(_state[2]);
-    pt1.y = cos(_state[2]);
+    if (!state) {
+        pt1.x = _length1 * sin(_theta1);
+        pt1.y = -_length1 * cos(_theta1);
 
-    pt2.x = pt1.x + sin(_state[3]);
-    pt2.y = pt1.y + cos(_state[2]);
+        pt2.x = pt1.x + (_length2 * sin(_theta2));
+        pt2.y = pt1.y - (_length2 * cos(_theta2));
+    } else {
+        pt1.x = _length1 * sin(_state[0]);
+        pt1.y = -_length1 * cos(_state[0]);
 
+        pt2.x = pt1.x + (_length2 * sin(_state[1]));
+        pt2.y = pt1.y - (_length2 * cos(_state[1]));
+    }
     vector<RectPoint> position;
     position.push_back(pt1);
     position.push_back(pt2);
@@ -320,6 +397,7 @@ vector<RectPoint> DoublePendulum::get_position() const {
 }
 
 int main() {
+    RectPoint o;
     int img_width;
     int img_height;
     int graph_width;
@@ -341,11 +419,39 @@ int main() {
     cin >> theta2;
 
     DoublePendulum dp(theta1, theta2, 1, 1, 1, 1);
-    //dp.RK4_step();
-    vector<RectPoint> test = dp.get_position();
-    
     Graph graph(img_width, img_height, graph_width, graph_height);
-    graph.clear_graph();
-    graph.draw_line(test[0], test[1]);
-    graph.write_image();
+
+    vector<RectPoint> test;
+
+    // for (int i = 0; i < 150; i++) {
+    //     test = dp.get_position();
+    //     //cout << "p1 = (" << test[0].x << ", " << test[0].y << ")" << endl;
+    //     //cout << "p2 = (" << test[1].x << ", " << test[1].y << ")" << endl;
+    //     graph.clear_graph();
+    //     graph.draw_line(o, test[0]);
+    //     graph.draw_line(test[0], test[1]);
+    //     string file_name = "Double_Pendulum_" + to_string(i) + ".png";
+    //     graph.write_image(file_name);
+
+    //     dp.update_d_omega();
+    // }
+
+    double dt = 0.01;
+    double total_time = 100;
+    double t = 0;
+    int frame = 0;
+
+    while (true) {
+        test = dp.get_position(1);
+        t += dt;
+        dp.update_state(t, dt);
+        graph.clear_graph();
+        graph.draw_line(o, test[0]);
+        graph.draw_line(test[0], test[1]);
+        string file_name = "Double_Pendulum_" + to_string(frame) + ".png";
+        graph.write_image(file_name);
+        frame++;
+    }
+    
+
 }
