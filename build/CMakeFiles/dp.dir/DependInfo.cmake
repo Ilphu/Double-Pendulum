2ba168a043f30b6eb8deff7d01cace83d8090597
Garrett <99564508+Ilphu@users.noreply.github.com>
2025-01-25 14:39:10 -0500

    better cmake

---
+++ build/CMakeFiles/dp.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/garrettrhoads/Documents/programmingProjects/CPP/Personal/Double-Pendulum/src/double_pendulum.cc" "CMakeFiles/dp.dir/src/double_pendulum.cc.o" "gcc" "CMakeFiles/dp.dir/src/double_pendulum.cc.o.d"
+  "/Users/garrettrhoads/Documents/programmingProjects/CPP/Personal/Double-Pendulum/src/graph.cc" "CMakeFiles/dp.dir/src/graph.cc.o" "gcc" "CMakeFiles/dp.dir/src/graph.cc.o.d"
+  "/Users/garrettrhoads/Documents/programmingProjects/CPP/Personal/Double-Pendulum/src/main.cc" "CMakeFiles/dp.dir/src/main.cc.o" "gcc" "CMakeFiles/dp.dir/src/main.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/dp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dp.dir/src/double_pendulum.cc.o"
+  "CMakeFiles/dp.dir/src/double_pendulum.cc.o.d"
+  "CMakeFiles/dp.dir/src/graph.cc.o"
+  "CMakeFiles/dp.dir/src/graph.cc.o.d"
+  "CMakeFiles/dp.dir/src/main.cc.o"
+  "CMakeFiles/dp.dir/src/main.cc.o.d"
+  "bin/dp"
+  "bin/dp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
